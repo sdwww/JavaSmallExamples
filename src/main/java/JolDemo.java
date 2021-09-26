c3c35da8c01ac52e129119a0fa9e81b6443fa7e7
wangweiweibj01 <wangweiweibj01@yuanfudao.com>
2021-09-26 11:27:35 +0800

    ADD: 1.object size

--- src/main/java/JolDemo.java
+++ src/main/java/JolDemo.java
@@ -1,5 +1,4 @@
 import org.openjdk.jol.info.ClassLayout;
-import org.springframework.stereotype.Service;
 
 /**
  * @author www
@@ -7,7 +6,18 @@ import org.springframework.stereotype.Service;
  */
 public class JolDemo {
     public static void main(String[] args) {
+
+        // 一个object占用16个字节，其中mark word占用8位，class pointer占用4位（压缩指针开启，未开启8位）
+        // 数据0位，填充
         Object object = new Object();
         System.out.println(ClassLayout.parseInstance(object).toPrintable());
+
+        // 一个boolean类型数组占用一个字节，length类型为int，占用4位
+        boolean[] booleans = new boolean[1024];
+        System.out.println(ClassLayout.parseInstance(booleans).toPrintable());
+
+        // 一个boolean类型占用一个字节
+        boolean b = false;
+        System.out.println(ClassLayout.parseInstance(b).toPrintable());
     }
 }

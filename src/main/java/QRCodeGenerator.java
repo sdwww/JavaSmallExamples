372b05179912043806ce77983f8c5b3792b303ac
wangweiweibj01 <wangweiweibj01@yuanfudao.com>
2021-10-12 17:38:23 +0800

    ADD: 1.二维码生成

---
+++ src/main/java/QRCodeGenerator.java
@@ -0,0 +1,43 @@
+/**
+ * @(#)QRCodeGenerator.java, 10月 12, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ * 生成二维码
+ *
+ * @author wangweiwei
+ */
+public class QRCodeGenerator {
+
+    public static void main(String[] args) {
+        try {
+            generateQRCodeImage("Hello World!!!", 500, 500, "QRTest.png");
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+
+
+}

1832f38e31ecec11e6662b569ea8f79ccf320209
wangweiwei <1043443209@qq.com>
2024-03-02 14:56:31 +0800

    1.局部性原理调整

--- src/main/java/LocalityDemo.java
+++ src/main/java/LocalityDemo.java
@@ -14,7 +14,7 @@
 public class LocalityDemo {
 
     public static void main(String[] args) {
-        int[][] nums1 = new int[5000][5000];
+        int[][] nums1 = new int[10000][10000];
         long startTime1 = System.currentTimeMillis();
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums1[0].length; j++) {
@@ -23,7 +23,7 @@ public class LocalityDemo {
         }
         System.out.println(System.currentTimeMillis() - startTime1);
 
-        int[][] nums2 = new int[5000][5000];
+        int[][] nums2 = new int[10000][10000];
         long startTime2 = System.currentTimeMillis();
         for (int j = 0; j < nums2[0].length; j++) {
             for (int i = 0; i < nums2.length; i++) {

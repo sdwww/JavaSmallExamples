912deaf10cad547749c37f9e78f78e317ee175f5
wangweiwei <1043443209@qq.com>
2024-11-03 19:38:33 +0800

    1.jetty例子

--- src/main/java/JettyDemo.java
+++ src/main/java/JettyDemo.java
@@ -1,10 +1,17 @@
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 
 public class JettyDemo {
 
     public static void main(String[] args) throws Exception {
 
         Server server = new Server(8080);
+
+        try (ServerConnector connector = new ServerConnector(server)) {
+            connector.setPort(8080);
+            server.setConnectors(new Connector[]{connector});
+        }
         server.start();
         server.join();
 
